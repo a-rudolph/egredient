671628196a9ec1e41a50709624fa4d7748af7932
Adam <adam.rudolph@live.ca>
2019-11-29 08:20:00 -0500

    purge message

--- tools/purge.js
+++ tools/purge.js
@@ -11,25 +11,33 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
     return;
   }
   dbo = db.db("e-gredient");
-  let collections = ["auth", "users", "sessions", "recipes", "ingredients"];
+  let collections = [
+    "auth",
+    "users",
+    "sessions",
+    "recipes",
+    "ingredients",
+    "done"
+  ];
   //   let collections = dbo.runCommand({
   //     listCollections: 1.0,
   //     authorizedCollections: true,
   //     nameOnly: true
   //   });
-  console.log("Collections: ", collections.join(", "));
+  // console.log("Collections: ", collections.join(", "));
   let inquire = () => {
     inquirer
       .prompt([
         {
-          type: "input",
+          type: "list",
+          choices: collections,
           name: "collection",
-          message: "What collection do you want to purge?"
+          message: "Choose a collection to purge?"
         }
       ])
       .then(answer => {
         if (answer.collection === "done") {
-          console.log("We hope you had a happy purge");
+          console.log("Happy coding!");
           db.close();
           return;
         }
