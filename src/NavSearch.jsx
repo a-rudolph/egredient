30effb971b961af85d39ea8c992135f6e8c8739c
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2019-12-05 21:44:57 -0500

    pretty sweet search node

--- src/NavSearch.jsx
+++ src/NavSearch.jsx
@@ -7,29 +7,70 @@ const Search = styled.div`
   justify-content: left;
   align-items: center;
   padding-left: 30px;
-
-  input {
-    border-radius: 0 0 0 5px;
+  label {
+    display: flex;
+    border: 1px solid;
+    border-radius: 15px;
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    opacity: ${props => props.opacity};
+    transition: opacity 0.2s ease;
   }
-  svg {
-    opacity: 0.3;
+  input {
+    padding: 5px 0;
+    width: 0;
+    border: 0px;
+    background: transparent;
+    transition: all 0.3s ease;
+    &:focus {
+      width: calc(100vw - 510px);
+    }
+  }
+  .svg {
+    width: 26px;
+    svg {
+      margin-left: 5px;
+    }
   }
 `;
 
 class NavSearch extends Component {
   constructor(props) {
     super(props);
-    this.state = { query: "" };
+    this.state = { query: "", open: false, opacity: "0.3" };
   }
 
+  changeHandler = ev => {
+    this.setState({ query: ev.target.value });
+  };
+  extend = () => {
+    this.setState({ open: true });
+  };
+  submitHandler = ev => {
+    ev.preventDefault();
+  };
   render() {
+    // console.log("rendering with state: ", this.state);
     return (
       <>
-        <Search>
+        <Search opacity={this.state.opacity}>
           <form onSubmit={this.submitHandler}>
-            <label>
-              <FaSearch />
+            <label
+              onClick={this.extend}
+              onMouseEnter={() => this.setState({ opacity: "0.8" })}
+              onMouseLeave={() => {
+                if (this.state.open) return;
+                this.setState({ opacity: "0.3" });
+              }}
+            >
+              <div className="bar svg">
+                <FaSearch />
+              </div>
               <input
+                className="bar extend"
                 type="text"
                 value={this.state.query}
                 onChange={this.changeHandler}
